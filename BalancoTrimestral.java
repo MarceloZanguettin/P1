71582a4e40f1e0ca182e4dd603c36be96472a054
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-04-04 16:43:54 +0000

    Resposta Primeira Questão

---
+++ BalancoTrimestral.java
@@ -0,0 +1,7 @@
+public class BalancoTrimestral{
+
+    public static void main(String[] args){
+        int gastosAbril = 1284;
+        System.out.println("O valor do salario minimo no estado de SP multiplicado 10x é: "+(gastosAbril*10)+" reais");
+    }
+}
